3e324c27e85293d6d518bf88add30d6583cf5b04
penne <penne0505pp@gmail.com>
2024-03-14 22:36:43 +0900

    delete logging library

--- src/main.py
+++ src/main.py
@@ -9,10 +9,8 @@ import components.views as ViewHandler
 import components.embeds as EmbedHandler
 
 
-
 intents = discord.Intents.all()
 handler = DBHandler()
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Client(discord.Client):
@@ -21,14 +19,14 @@ class Client(discord.Client):
         self.synced = False
     
     async def on_ready(self):
-        logging.info(Fore.YELLOW + 'Bot is starting...' + Style.RESET_ALL)
-        logging.info(Fore.YELLOW + 'Syncing commands...' + Style.RESET_ALL)
+        print(Fore.YELLOW + 'Bot is starting...' + Style.RESET_ALL)
+        print(Fore.YELLOW + 'Syncing commands...' + Style.RESET_ALL)
         if not self.synced:
             await self.sync_commands()
-        logging.info(Fore.GREEN + 'Commands synced' + Style.RESET_ALL)
+        print(Fore.GREEN + 'Commands synced' + Style.RESET_ALL)
         await self.set_presence()
-        logging.info(Fore.BLUE + f'Logged in as {self.user.name} ({self.user.id})' + Style.RESET_ALL)
-        logging.info(Fore.GREEN + 'Bot is ready' + Style.RESET_ALL)
+        print(Fore.BLUE + f'Logged in as {self.user.name} ({self.user.id})' + Style.RESET_ALL)
+        print(Fore.GREEN + 'Bot is ready' + Style.RESET_ALL)
     
     async def sync_commands(self):
         if self.synced:
